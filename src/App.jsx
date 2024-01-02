b8efeb84493e772d33cf70445cc552d77d95d1f3
Eder Henrique <ederhscc@gmail.com>
2024-01-02 14:39:20 -0300

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CurrencyConverter from "./components/CurrencyConverter";
+
+function App() {
+  return (
+    <>
+      <CurrencyConverter />
+    </>
+  );
+}
+
+export default App;
+// 8d0a843eba7c20462bf8a354
---
+++ src/components/CurrencyConverter.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { axios } from "axios";
+
+import "./CurrencyConverter.css";
+
+const CurrencyConverter = () => {
+  return <div>CurrencyConverter</div>;
+};
+
+export default CurrencyConverter;
